93dc14cd490586bcd10c1a51ec17c0f81e9f53da
syflex360 <syflex360@gmail.com>
2019-08-10 15:34:41 +0100

    scaffold and authentication

---
+++ src/components/login/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+  <q-card class="my-card">
+  <q-card-section class="q-gutter-sm">
+    <q-input square dense="" outlined v-model="form.email" label="Email" />
+    <q-input square dense="" outlined v-model="form.password" label="Password" />
+    <q-btn color="white" text-color="black" label="Login" @click="login"/>    
+  </q-card-section>
+  </q-card>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {
+      form:{
+        email: '',
+        password: '',
+      }
+    }
+  },
+
+  created() {
+    
+  },
+
+  methods: {
+    async login() {
+      console.log(process.env.SECRET);
+      
+      try {
+        const res = await this.$axios.post(process.env.API + '/api/login', this.form)
+      } catch (error) {
+        
+      }
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <login />
+  </q-page>
+</template>
+
+<script>
+import login from 'components/login/login'
+export default {
+  name: 'Login',
+  components:{
+    login
+  }
+}
+</script>
+
+<style>
+</style>
