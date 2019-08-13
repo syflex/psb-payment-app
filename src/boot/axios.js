f2585e0dc99b86d1127982333b4eb76fee7cefa7
syflex360 <syflex360@gmail.com>
2019-08-13 23:32:43 +0100

    Add useful message

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default async ({ Vue }) => {
   Vue.prototype.$axios = axios;
-  axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.SECRET}`;
+  axios.defaults.headers.common['Authorization'] = `Bearer sk_test_eaa2518cbcfbd9ba28c6d984895b401930e0484e`;
 }
